22c2e1a2312865ec984d5112f29db6d6817731b2
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-07 16:14:04 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+author 'EthanPeacock'
+description 'progressBars'
+version '1.0.0'
+
+ui_page 'h.html'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'h.html'
+}
+
+export "startUI"
